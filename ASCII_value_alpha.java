225fde941890b1dc76acda8f839d5ec420325c6e
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-25 23:23:09 +0530

    (easy) practice question

---
+++ ASCII_value_alpha.java
@@ -0,0 +1,10 @@
+public class ASCII_value_alpha {
+    public static void main(String[] args) {
+        char i ='a';
+        while(i<='z'){
+            int x=i;
+            System.out.println(i+" "+x);
+            i++;
+        }
+    }
+}
